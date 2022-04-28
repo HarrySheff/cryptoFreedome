7aed6a46efa138d38799978bb314d24f815b6a31
Уманцев Игорь Валерьевич <iumantsev@at-consulting.ru>
2022-04-28 13:31:30 +0300

    Index page text fix

--- src/assets/js/indexPageStrings.js
+++ src/assets/js/indexPageStrings.js
@@ -359,8 +359,8 @@ let indexPageStrings={
 
 	'i-mem-p1':{
 		selector:"#i-mem-p1",
-		"en": 'There are 3 consecutive Bonus plans Light, Smart and Prime with 16 Levels in each of them. To register, activate the next Bonus Plan or upgrade an it\'s Level, a Slot of the corresponding Bonus Plan and Level is required.',
-		"ru": 'Есть 3 последовательных Бонусных плана Light, Smart и Prime по 16 Уровней в каждом из них. Для регистрации, активации следующего Бонусного плана или повышения его Уровня требуется Слот соответствующего Бонусного плана и Уровня.' 
+		"en": 'There are 3 consecutive Bonus plans Light, Smart and Prime with 16 Levels in each of them. <span class="text-warning">To register</span>, activate the next Bonus Plan or upgrade an it\'s Level, <span class="text-warning">a Slot</span> of the corresponding Bonus Plan and Level <span class="text-warning">is required</span>.',
+		"ru": 'Есть 3 последовательных Бонусных плана Light, Smart и Prime по 16 Уровней в каждом из них. <span class="text-warning">Для регистрации</span>, активации следующего Бонусного плана или повышения его Уровня <span class="text-warning">требуется Слот</span> соответствующего Бонусного плана и Уровня.' 
 	},
 
 	'i-mem-p2':{
